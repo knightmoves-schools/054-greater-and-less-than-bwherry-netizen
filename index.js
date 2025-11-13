89e274205b574ef72ab2b52acfde85bd74564dfb
Brendon W <bwherry@midland.k12.ia.us>
2025-11-13 17:58:48 +0000

    .5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function oldEnough (age){
+    if (age > 17) {
+        return "old enough";
+    }
+
+    if (age <= 17) {
+        return null;
+    }
+}
+
+
+function underAge (age){
+    if (age < 18) {
+        return "under age";
+    }
+
+    if (age >= 18) {
+        return null;
+    }
+}
+
+
+
+
+
+
 
